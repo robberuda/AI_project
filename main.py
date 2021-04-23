59b1ba4d46efc2c1808c8334e7c8e9ae74ad4124
robberuda <robberuda@gmail.com>
2021-04-23 20:09:16 +0200

    found first solution

--- main.py
+++ main.py
@@ -1,49 +1,97 @@
 import moves
+import time
 
+goal = 'WWWWGGOOBBRRGGOBRRYYY'
 
+start = 'WRGRBYGYOBYOOWGBWWGRR'
+
+start2mosse= 'WWGGOOBWRRYGYGOWRRBYB'
+
+moves_index = {0:'U', 1:'R', 2:'L', 3:'D', 4:'F', 5:'B'}
+
+#definition of node
 class node:
-    def __init__(self, value=None):
+    def __init__(self, value=None, move=None, parent=None):
         self.value = value
-        self.move = None
-        self.parent = None  # pointer to parent node in tree
+        self.move = move
+        self.parent = parent  # pointer to parent node in tree
+        self.depth = None
 
 
+#definition of search tree
 class search_tree:
     def __init__(self):
         self.root = None
+        self.elements = []
+        self.leaves = []
+        self.max_depth = None
+        self.solution = None
+        self.solutionNode=None
 
-    def insert(self, node):
-        self.root == None:
-            self.root = node
+    def add_root(self, root):
+        if self.root == None:
+            self.root = root
+            self.elements = [root]
+            self.leaves = [root]
+            self.max_depth = 0
+            self.solution = 0
 
-    def generate_child_of(self, node):
-        #
+    def get_children(self, father):
+        results = [moves.F(father.value), moves.Fp(father.value), moves.R(father.value), moves.Rp(father.value), moves.U(father.value), moves.Up(father.value)]
+        children = []
+        for i in range(len(results)):
+            if results[i] not in self.elements:
+                n = node(results[i])
+                n.move = moves_index[i]
+                n.parent = father
+                n.depth = father.depth + 1
+                self.elements.append(n)
+                self.leaves.append(n)
+                if n.depth >= self.max_depth:
+                    self.max_depth = n.depth
+                if n.value == goal:
+                    self.solution = 1
+                    self.solutionNode = n
+        self.leaves.remove(father)
+        
+    def print_tree(self):
+        print('max depth is: ', self.max_depth)
+        for i in range(self.max_depth+1):
+            cnt=0
+            for el in self.elements:
+                if el.depth == i:
+                    cnt +=1
+            print(i,': ', cnt)
 
+    def expand_tree(self):
+        for el in self.leaves:
+            if el.depth < 9:
+                #print('leaves number: ', len(self.leaves))
+                self.get_children(el)
+                if (self.solution):
+                    print('SOLUTION FOUND')
+                    self.print_solution(self.solutionNode)
+                    break
+            else: break
 
+    def print_solution(self, n):
+        for d in range(n.depth):
+            print(n.move)
+            n = n.parent
 
+#create root node
+root = node(start2mosse)
+root.depth = 0
 
-goal = 'WWWWGGRRBBOOGGRRBBOOYYYY'
-
-start = 'WBROWBYOWGRBGWGRBRYOYOGY'
-
-
-root = node()
-root = node(start)
-
+#create search tree
 tree = search_tree()
-tree.insert(root)
 
+#add root
+tree.add_root(root)
 
+start_time = time.time()
+tree.expand_tree()
+print('solved in ' + str(round(time.time() - start_time, 2)) + ' seconds')
 
-
-
-def get_moves(pos):
-    return [moves.U(pos), moves.R(pos), moves.L(pos), moves.D(pos), moves.F(pos), moves.B(pos)]
-
-
-# depth limited:
-def findSolution(pos):
-    if (pos == goal): return "it's already the solution"
-
-
-print(tree.root.value)
+#grenerate and print children
+tree.print_tree()
--- moves.py
+++ moves.py
@@ -1,66 +1,69 @@
 # Hard coded moves to perform on the cube.
 # Position is passed in and returned with the stickers in the order they should be after the turn.
 
-# Up Quarter Turn Clockwise
-def noMove(pos):
-	return (pos[0]  + pos[1] +
-	        pos[2]  + pos[3] +
-	        pos[4]  + pos[5]  + pos[6]  + pos[7]  + pos[8]  + pos[9]  + pos[10] + pos[11] +
-	        pos[12] + pos[13] + pos[14] + pos[15] + pos[16] + pos[17] + pos[18] + pos[19] +
-			pos[20] + pos[21] +
-			pos[22] + pos[23])
 
+moves_index = {0:'F', 1:'Fp', 2:'R', 3:'Rp', 4:'U', 5:'Up'}
 
-# Up Quarter Turn Clockwise
-def U(pos):
-	return (pos[2]  + pos[0] +
-			pos[3]  + pos[1] +
-			pos[6]  + pos[7] + pos[8] + pos[9] + pos[10] + pos[11] + pos[4] + pos[5] +
-			pos[12] + pos[13] + pos[14] + pos[15] + pos[16] + pos[17] + pos[18] + pos[19] +
-			pos[20] + pos[21] +
-			pos[22] + pos[23])
-
-# Right Quarter Turn Clockwise
-def R(pos):
-	return (pos[0]  + pos[5]  +
-			pos[2]  + pos[13] +
-			pos[4]  + pos[21] + pos[14] + pos[6] + pos[3]  + pos[9]  + pos[10] + pos[11] +
-			pos[12] + pos[23] + pos[15] + pos[7] + pos[1]  + pos[17] + pos[18] + pos[19] +
-			pos[20] + pos[16] +
-			pos[22] + pos[8])
-
-# Left Quarter Turn Clockwise
-def L(pos):
-	return (pos[17] + pos[1] +
-	        pos[9] + pos[3] +
-	        pos[0]  + pos[5]  + pos[6]  + pos[7]  + pos[8]  + pos[22]  + pos[18] + pos[10] +
-	        pos[2]  + pos[13] + pos[14] + pos[15] + pos[16] + pos[20] + pos[19] + pos[11] +
-			pos[4]  + pos[21] +
-			pos[12] + pos[23])
-
-# Down Quarter Turn Clockwise
-def D(pos):
-	return (pos[0]  + pos[1] +
-	        pos[2]  + pos[3] +
-	        pos[4]  + pos[5]  + pos[6]  + pos[7]  + pos[8]  + pos[9]  + pos[10] + pos[11] +
-	        pos[18] + pos[19] + pos[12] + pos[13] + pos[14] + pos[15] + pos[16] + pos[17] +
-			pos[22] + pos[20] +
-			pos[23] + pos[21])
+# Hard coded moves to perform on the cube.
+# Position is passed in and returned with the stickers in the order they should be after the turn.
 
 # Front Quarter Turn Clockwise
 def F(pos):
-	return (pos[0]  + pos[1] +
-	        pos[19] + pos[11] +
-	        pos[12] + pos[4] + pos[2]  + pos[7]  + pos[8]  + pos[9]  + pos[10] + pos[20] +
-	        pos[13] + pos[5] + pos[3] + pos[15] + pos[16] + pos[17] + pos[18] + pos[21] +
-			pos[15] + pos[14] +
-			pos[22] + pos[23])
+	return (pos[6] + pos[14] + pos[2] +
+			pos[3] + pos[5] + pos[13] +
+			pos[19] + pos[7] + pos[8] +
+			pos[9] + pos[10] + pos[1] +
+			pos[4] + pos[12] + pos[18] +
+			pos[15] + pos[16] + pos[0] +
+			pos[11] + pos[17] + pos[20])
 
-# Back Quarter Turn Clockwise
-def B(pos):
-	return (pos[7]  + pos[15] +
-	        pos[2]  + pos[3] +
-	        pos[4]  + pos[5]  + pos[6]  + pos[23] + pos[16] + pos[8]  + pos[1] + pos[11] +
-	        pos[12] + pos[13] + pos[14] + pos[22] + pos[17] + pos[9] + pos[0] + pos[19] +
-			pos[20] + pos[21] +
-			pos[10] + pos[18])
+# Front Quarter Turn Counterclockwise
+def Fp(pos):
+	return (pos[17] + pos[11] + pos[2] +
+			pos[3] + pos[12] + pos[4] +
+			pos[0] + pos[7] + pos[8] +
+			pos[9] + pos[10] + pos[18] +
+			pos[13] + pos[5] + pos[1] +
+			pos[15] + pos[16] + pos[19] +
+			pos[14] + pos[6] + pos[20])
+
+
+# Right Quarter Turn Clockwise
+def R(pos):
+	return (pos[12] + pos[1] + pos[2] +
+			pos[4] + pos[18] + pos[5] +
+			pos[6] + pos[7] + pos[8] +
+			pos[0] + pos[11] + pos[17] +
+			pos[20] + pos[13] + pos[14] +
+			pos[3] + pos[10] + pos[16] +
+			pos[15] + pos[19] + pos[9])
+
+# Right Quarter Turn Counterclockwise
+def Rp(pos):
+	return (pos[9] + pos[1] + pos[2] +
+			pos[15] + pos[3] + pos[5] +
+			pos[6] + pos[7] + pos[8] +
+			pos[20] + pos[16] + pos[10] +
+			pos[0] + pos[13] + pos[14] +
+			pos[18] + pos[17] + pos[11] +
+			pos[4] + pos[19] + pos[12])
+
+# Up Quarter Turn Clockwise
+def U(pos):
+	return (pos[3] + pos[0] + pos[1] +
+			pos[2] + pos[10] + pos[11] +
+			pos[4] + pos[5] + pos[6] +
+			pos[7] + pos[8] + pos[9] +
+			pos[12] + pos[13] + pos[14] +
+			pos[15] + pos[16] + pos[17] +
+			pos[18] + pos[19] + pos[20])
+
+# Up Quarter Turn Counterclockwise
+def Up(pos):
+	return (pos[1] + pos[2] + pos[3] +
+			pos[0] + pos[6] + pos[7] +
+			pos[8] + pos[9] + pos[10] +
+			pos[11] + pos[4] + pos[5] +
+			pos[12] + pos[13] + pos[14] +
+			pos[15] + pos[16] + pos[17] +
+			pos[18] + pos[19] + pos[20])
